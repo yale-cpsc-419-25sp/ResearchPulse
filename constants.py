fde19ecb416982c9b58e2962c536cbacc9e40c85
TsachMa <tcmackey.md@gmail.com>
2025-02-26 18:39:24 -0500

    test input for debugging the database populator

---
+++ constants.py
@@ -0,0 +1,24 @@
+test_input = [
+        {
+            "doi": "10.1038/s41586-2023-1234",
+            "title": "Novel biomarkers in breast cancer progression",
+            "venue": "Nature",
+            "year": "2023",
+            "authors": [
+                {
+                    "authorId": "12345",
+                    "name": "Jane Smith",
+                    "affiliations": [
+                        {"name": "Stanford University"}
+                    ]
+                },
+                {
+                    "authorId": "67890",
+                    "name": "John Doe",
+                    "affiliations": [
+                        {"name": "Harvard Medical School"}
+                    ]
+                }
+            ]
+        }
+    ]
