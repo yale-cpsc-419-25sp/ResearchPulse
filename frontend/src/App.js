f2b469f9e1cfd842139bcc6962f38742ce459506
mogura5 <manaka.ogura@yale.edu>
2025-03-19 17:03:20 -0700

    Created React App

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+// import { login } from '.login.js';
+
+// function handleLogin(user_id) {
+//   const id_result = login(user_id);
+
+//   if (id_result.success)
+// }
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
